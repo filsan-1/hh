66f121ff23ea454a20f01bbffcdbacd855293be3
fh <128391719+filsan-1@users.noreply.github.com>
2024-05-27 08:39:58 -0700

    Add FastAPI service and model/scaler files

---
+++ fastapi_service/main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import pickle
+import os
+
+class Parameters(BaseModel):
+    age: float
+    marriageStatus: float
+    weight: float
+    bmi: float
+    follicleNoR: float
+    follicleNoL: float
+    amh: float
+    regularCycle: float
+    cycleLength: float
+    skinDarkening: float
+    hairGrowth: float
+    weightGain: float
+    fastFood: float
+    pimples: float
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/")
+async def get_prediction(patientParameters: Parameters):
+    predictionParams = [
+        patientParameters.skinDarkening,
+        patientParameters.hairGrowth,
+        patientParameters.weightGain,
+        patientParameters.fastFood,
+        patientParameters.pimples,
+        patientParameters.follicleNoR,
+        patientParameters.follicleNoL,
+        patientParameters.regularCycle,
+        patientParameters.amh,
+        patientParameters.weight,
+        patientParameters.bmi,
+        patientParameters.cycleLength,
+        patientParameters.age,
+        patientParameters.marriageStatus,
+    ]
+
+    loaded_model = pickle.load(open(os.path.join(os.getcwd(), "model.sav"), "rb"))
+    sc = pickle.load(open(os.path.join(os.getcwd(), "sc.sav"), "rb"))
+
+    predictionParams = sc.transform([predictionParams])
+    res = loaded_model.predict(predictionParams)
+
+    if res:
+        return {"atRisk": True}
+    else:
+        return {"atRisk": False}
