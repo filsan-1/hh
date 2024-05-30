f4b1070b73d9e430d4679125a3431e0c4abfbd18
fh <128391719+filsan-1@users.noreply.github.com>
2024-05-30 15:46:10 -0700

    Update models.py

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+
+class PeriodLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    cycle_length = models.IntegerField()
+
+class SymptomLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    symptom = models.CharField(max_length=100)
+    severity = models.IntegerField(choices=[(i, i) for i in range(1, 11)])
+    notes = models.TextField(blank=True, null=True)
+
 
 # Create your models here.
