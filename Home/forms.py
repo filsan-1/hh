681b92b0fffc06023b669c4826782dab0f63ac84
fh <128391719+filsan-1@users.noreply.github.com>
2024-05-30 05:55:23 -0700

    Update forms.py

--- Home/forms.py
+++ Home/forms.py
@@ -3,7 +3,16 @@ from django import forms
 class ContactForm(forms.Form):
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
-    email_address = forms.EmailField(max_length=150)  # Changed to EmailField
+    email_address = forms.EmailField(max_length=150)
     message = forms.CharField(widget=forms.Textarea, max_length=2000)
+
+class PeriodForm(forms.Form):
+    # Define the fields for the PeriodForm
+    pass
+
+class SymptomForm(forms.Form):
+    # Define the fields for the SymptomForm
+    pass
+
 
     
