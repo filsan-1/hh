f57d356159579e0c5bfd78ae8bb81ecfaa5e6993
fh <128391719+filsan-1@users.noreply.github.com>
2024-06-02 05:33:09 -0700

    Update admin.py

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Period, Symptom
+
+class SymptomInline(admin.TabularInline):
+    model = Symptom
+    extra = 1
+
+class PeriodAdmin(admin.ModelAdmin):
+    inlines = [SymptomInline]
+
+admin.site.register(Period, PeriodAdmin)
+admin.site.register(Symptom)
 
 # Register your models here.
